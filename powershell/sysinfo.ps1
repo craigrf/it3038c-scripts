362a40c371acff05c33a705c9ea4a5172d82c56c
craigrf <craigrf@mail.uc.edu>
2023-02-05 12:17:12 -0500

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -2,7 +2,28 @@
     (Get-NetIPAddress).IPv4Address | Select-String "192*"
 }
 
-Write-Host(getIP)
+function getUser{
+    $env:USERNAME
+}
+
+function getHostname{
+    Hostname
+}
+
+function getVersion{
+    $Host.Version.Major
+}
+
+function getDate{
+    Get-Date -UFormat "%A, %B %e, %Y"
+}
 
 $IP = getIP
-Write-Host("This machine's IP is $IP")
+$User = getUser
+$Hostname = getHostname
+$Version = getVersion
+$Date = getDate
+
+$Body = "This machine's IP address is $IP. The user is $User. The hostname is $Hostname. PowerShell is on version $Version. Today's date is $Date."
+
+Send-MailMessage -To "craigrf@mail.uc.edu" -From "rcraig1999@gmail.com" -Subject "Riley Craig IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
