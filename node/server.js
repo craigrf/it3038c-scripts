701bb545b4529f0845cfb22a85260992f286e685
craigrf <craigrf@mail.uc.edu>
2023-02-26 15:15:16 -0500

    lab 6

--- node/server.js
+++ node/server.js
@@ -10,7 +10,39 @@ http.createServer((req, res) => {
       res.end(body);
     });
   } else if(req.url.match("/sysinfo")) {
+
+    // formats uptime output to days, hours, minutes, seconds
+    var ut_sec = os.uptime();
+    var ut_min = ut_sec/60;
+    var ut_hour = ut_min/60;
+    var ut_day = ut_hour/24;
+    ut_sec = Math.floor(ut_sec);
+    ut_min = Math.floor(ut_min);
+    ut_hour = Math.floor(ut_hour);
+    ut_day = Math.floor(ut_day);
+    ut_sec = ut_sec%60
+    ut_min = ut_min%60
+    ut_hour = ut_hour%24
+    ut_day = ut_day%60
+
+    // formats free memory from bytes to MB
+    var free = os.freemem() / 1024 / 1024;
+    free = Math.floor(free);
+
+    // formats total memory from bytes to MB
+    var total = os.totalmem() / 1024 / 1024;
+    total = Math.floor(total);
+
+    // calculates total number of CPUs
+    const numCPU = os.cpus().length;
+
+    // outputs for site
     myHostName=os.hostname();
+    myUptime=(ut_day + " Day(s), " + ut_hour + " Hour(s), " + ut_min + " Minute(s), " + ut_sec + " Second(s)"); // outputs uptime
+    memFree=(free + " MB");
+    totalMemory=(total + " MB");
+    totalCPU=(numCPU);
+
     html=`
     <!DOCTYPE html>
     <html>
@@ -20,10 +52,10 @@ http.createServer((req, res) => {
       <body>
         <p>Hostname: ${myHostName}</p>
         <p>IP: ${ip.address()}</p>
-        <p>Server Uptime: </p>
-        <p>Total Memory: </p>
-        <p>Free Memory: </p>
-        <p>Number of CPUs: </p>
+        <p>Server Uptime: ${myUptime}</p>
+        <p>Total Memory: ${totalMemory}</p>
+        <p>Free Memory: ${memFree}</p>
+        <p>Number of CPUs: ${totalCPU}</p>
       </body>
     </html>`
     res.writeHead(200, {"Content-Type": "text/html"});
--- node/test.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-const hello = "Hello from Node JS Variable!"
-console.log(hello);
-console.log("Using PATH module:");
-console.log(`Hello from file ${path.basename(__filename)}`);
-console.log(`Progress args: ${process.argv}`);
